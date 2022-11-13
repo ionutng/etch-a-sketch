e405fda30b2cef827ea2affa5de895bf1712b30a
maahesi <ionutngp@gmail.com>
2022-11-13 17:18:26 +0200

    Add a button for changing the grid size

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function createGrid(size) {
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+    const squares = container.querySelectorAll('div');
+    squares.forEach(squareDiv => squareDiv.remove());
+
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
             const square = document.createElement('div');
@@ -17,4 +20,16 @@ createGrid(16);
 
 function changeColor() {
     this.style.backgroundColor = "#175a84";
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', changeSize);
+
+function changeSize() {
+    const size = prompt('Size? Should be at least 2, but not bigger than 100');
+    if (size >= 2 && size <= 100) {
+        createGrid(size);
+    } else {
+        console.log("The size should be at least 2 and not bigger than 100");
+    }
 }
